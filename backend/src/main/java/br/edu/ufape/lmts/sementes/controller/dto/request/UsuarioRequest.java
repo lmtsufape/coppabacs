f7488d336ca62b642d4f583e5d9cb4c9b7fc8a33
LuoLuann <luo.luann@gmail.com>
2024-01-10 16:23:03 -0300

    add restricoes ao request de usuario

--- backend/src/main/java/br/edu/ufape/lmts/sementes/controller/dto/request/UsuarioRequest.java
+++ backend/src/main/java/br/edu/ufape/lmts/sementes/controller/dto/request/UsuarioRequest.java
@@ -2,7 +2,6 @@ package br.edu.ufape.lmts.sementes.controller.dto.request;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Set;
 
 import org.modelmapper.ModelMapper;
 
@@ -10,6 +9,9 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import br.edu.ufape.lmts.sementes.config.SpringApplicationContext;
 import br.edu.ufape.lmts.sementes.model.Usuario;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,20 +19,32 @@ import lombok.Setter;
 
 @Getter @Setter @NoArgsConstructor @JsonPropertyOrder
 public  class UsuarioRequest  {
-
+	
+	@NotBlank
 	private String nome;
+	@NotBlank @Email
 	private String email;
+	@NotBlank
 	private String senha;
 	private EnderecoRequest endereco;
+	@NotBlank
 	private String rg;
+	@NotBlank @Size(max = 14)
 	private String cpf;
+	@NotBlank
 	private LocalDate dataNascimento;
+	@NotBlank
 	private String contato;
 	private String imagem;
+	@NotBlank
 	private String nomePai;
+	@NotBlank
 	private String nomeMae;
+	@NotBlank
 	private String nis;
+	@NotBlank
 	private String tituloEleitor;
+	@NotBlank
 	private String sexo;
 	private ConjugeRequest conjuge;
 	private List<PostavelRequest> postavel;
