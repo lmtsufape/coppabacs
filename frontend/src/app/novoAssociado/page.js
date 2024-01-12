b6b8e0cb94036b8a071417298783711bbdb1b100
DougM244 <douglas.leitemorais@gmail.com>
2024-01-11 22:17:08 -0300

    :sparkles: feat: Página de cadastro de um novo banco

--- frontend/src/app/novoAssociado/page.js
+++ frontend/src/app/novoAssociado/page.js
@@ -2,15 +2,25 @@ import React from "react";
 import NewFarmerForm from "@/components/NewFarmerForm/indexOne";
 import Footer from "@/components/Home/Footer";
 import Header from "@/components/Home/Header";
+import styles from "@/app/novoAssociado/index.module.scss"
 
 export default function NewRegisterFarmer() {
     return (
         <div>
+            <div>
             <Header />
+            </div>
+            <div>
+                <h1 className={styles.title}>
+                    Atividade Rural
+                </h1>
+            </div>
             <div>
                 <NewFarmerForm />
             </div>
+            <div>
             <Footer />
+            </div>
         </div>
     );
 }
--- frontend/src/components/FormPattern/Forms/Label/label.js
+++ frontend/src/components/FormPattern/Forms/Label/label.js
@@ -3,7 +3,7 @@ import styles from "@/components/FormPattern/Forms/forms.module.css"
 export default function Label({text}){
     return(
         <div className={styles.labelStandard}>
-        <label>{text}</label>
+            <label>{text}</label>
         </div>
     )
 }
