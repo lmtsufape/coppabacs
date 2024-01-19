fb24d4df24866e5766ae784d39e5c422dcb73150
LuoLuann <luo.luann@gmail.com>
2024-01-19 12:19:18 -0300

    feat: add funcao para pegar usuario logado

--- backend/src/main/java/br/edu/ufape/lmts/sementes/controller/AuthController.java
+++ backend/src/main/java/br/edu/ufape/lmts/sementes/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,5 +37,9 @@ public class AuthController {
 		Authentication auth = this.authenticationManager.authenticate(userNamePassword);
 		String token = tokenService.generateToken((AuthUser) auth.getPrincipal());
 		return ResponseEntity.ok().header("Authorization", "Bearer " + token).header("access-control-expose-headers", "Authorization").build();
+	}
+	@PostMapping("user/current")
+	public Authentication getCurrentUser() {
+		return SecurityContextHolder.getContext().getAuthentication();
 	}
 }
