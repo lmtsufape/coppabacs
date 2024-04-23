96da07d85da14b3a8f032a06752d2c9a0505d54f
magno <magno.sillas0@gmail.com>
2024-04-23 20:12:36 -0300

    alteracoes lmts

--- backend/src/main/java/br/edu/ufape/lmts/sementes/controller/dto/request/DoacaoUsuarioRequest.java
+++ backend/src/main/java/br/edu/ufape/lmts/sementes/controller/dto/request/DoacaoUsuarioRequest.java
@@ -30,7 +30,8 @@ public  class DoacaoUsuarioRequest  {
 		BancoSementes bancoSementes = new BancoSementes();
 		bancoSementes.setId(bancoSementesId);
 		obj.setBancoSementes(bancoSementes);
-				return obj;
+
+		return obj;
 	}
 
 
