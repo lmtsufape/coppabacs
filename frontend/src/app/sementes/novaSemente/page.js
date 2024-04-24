d1cf5d80069a2b47f8a4d93f49c814f6d57a492e
Giudicelli Elias <86128363+giudicellisilva@users.noreply.github.com>
2024-04-24 03:28:26 -0300

    Alterações

--- frontend/src/app/sementes/novaSemente/page.js
+++ frontend/src/app/sementes/novaSemente/page.js
@@ -1,6 +1,5 @@
 import React from "react";
 import SementeForm from "@/components/SementeForm/index";
-import Footer from "@/components/Footer";
 
 export default function NovaSementeFormPage() {
     return (
