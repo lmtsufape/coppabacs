288faa53d30ba604d338391c7708281f6507312a
Giudicelli Elias <86128363+giudicellisilva@users.noreply.github.com>
2024-04-24 03:28:26 -0300

    Alterações

--- frontend/src/app/mural/criarPostagem/page.js
+++ frontend/src/app/mural/criarPostagem/page.js
@@ -4,7 +4,6 @@ import CriarPostagem from "@/components/Mural/CriarPostagem/index";
 export default function criarPostagemPage() {
     return (
         <div>
-
             <CriarPostagem
                 diretorioAnterior="Home / Mural / "
                 diretorioAtual="Criar Postagem"
