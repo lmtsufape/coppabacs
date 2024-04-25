12d7a4fa1481261b80e853e33105d0c8ba79a3cd
Douglas Morais <douglas.leitemorais@gmail.com>
2024-04-25 16:27:05 -0300

    correção de botões e resposividade da tela

--- frontend/src/components/ListSementes/index.js
+++ frontend/src/components/ListSementes/index.js
@@ -48,9 +48,7 @@ export default function List({ diretorioAnterior, diretorioAtual, hrefAnterior, 
         <div className={styles.header__container}>
           {role ? <button>
             <Link className={styles.header__container_link} href="sementes/novaSemente">
-              <h1>
-                Adicionar Sementes
-                </h1>
+              <h1>Adicionar Semente</h1>
             </Link>
             <Image src="/assets/iconSeedGrey+.svg" width={20} height={20} />
           </button> : ""}
