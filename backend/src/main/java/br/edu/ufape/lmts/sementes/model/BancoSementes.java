04f6d12eaa3b4d8af4586388ecfedafb519226cd
Magno Sillas <magno.sillas0@gmail.com>
2024-05-02 12:26:09 -0300

    rotas publicas + busca sementes por banco + consertando relacoes

--- backend/src/main/java/br/edu/ufape/lmts/sementes/model/BancoSementes.java
+++ backend/src/main/java/br/edu/ufape/lmts/sementes/model/BancoSementes.java
@@ -81,6 +81,19 @@ public class BancoSementes {
 		this.transacaoGenerica.add(transacaoGenerica);
 	}
 
+	public void addDoacaoUsuario(DoacaoUsuario doacaoUsuario){
+		this.doacaoUsuario.add(doacaoUsuario);
+	}
+
+	public void addRetiradaUsuario(RetiradaUsuario retiradaUsuario){
+		this.retiradaUsuario.add(retiradaUsuario);
+	}
+
+	public void addTransacaoGenerica(TransacaoGenerica transacaoGenerica){
+		this.transacaoGenerica.add(transacaoGenerica);
+	}
+
+
 	public long getId() {
 		return id;
 	}
