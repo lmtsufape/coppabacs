f1210ad0d857998a24bea82438250090b23592aa
Douglas Morais <douglas.leitemorais@gmail.com>
2024-05-22 19:44:02 -0300

    feat: adição de cards nas telas de listagem

--- frontend/src/components/ListAgricultores/Table/index.js
+++ frontend/src/components/ListAgricultores/Table/index.js
@@ -64,29 +64,17 @@ export default function tableLayout({ table1, table2, table3, table4, listAgricu
       <div className={style.contentSmall}>
         <div className={style.content}>
           <table className={style.content__table}>
-            <thead className={style.content__table__header}>
-              <tr>
-                <th>{table1}</th>
-                <th>{table2}</th>
-                <th className={style.content__table__header_name3}>
-                  <div >
-                    {table3}
-                    <Image src="/assets/iconInformacao.svg" alt="Visualizar" width={27} height={26} />
-
-                  </div>
-
-                </th>
-              </tr>
-            </thead>
             <tbody className={style.content__table__body}>
               {listAgricultores.map((agricultor, index) => {
                 return (
                   <tr key={index}>
-                    <td>{agricultor.nome}</td>
-                    <td>{agricultor.nomePopular}</td>
-                    <td>
-                    <div >
-                        <Image src="/assets/iconOlho.svg" onClick={() => onSelectAgricultor(agricultor)} alt="Visualizar" width={27} height={26} />
+                    <td data-label="Nome">{agricultor.nome}</td>
+                    <td data-label="Apelido">{agricultor.nomePopular}</td>
+                    <td data-label="Telefone">{agricultor.contato}</td>
+                    <td className={style.content__table__buttonTabela}>
+                    <div className={style.content__table__button}>
+                      <h1>Visualizar</h1>
+                        <Image className={style.content__table__button_img} src="/assets/iconOlhoBranco.png" onClick={() => onSelectAgricultor(agricultor)} alt="Visualizar" width={24} height={26} />
 
                         <ExcluirButton itemId={agricultor.id} onDelete={handleDeleteAgricultor} />
 
